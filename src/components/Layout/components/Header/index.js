c44344218f0ad58f76bcbc1376d2819bf88bd943
TruongCaoAnhHuy <huy96qt123@gmail.com>
2023-04-01 23:08:42 +0700

    Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss'
+import images from '~/assets/img';
 
 const cx = classNames.bind(styles)
+
+console.log(images.logo.default);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt='tiktok' />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Search accouts and videos' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+
+                </div>
+                <div className={cx('action')}>
+
+                </div>
+            </div>
         </header>
     )
 }
